077e6486bc8f8450c1134aa5e65a3120e2476c93
YoonJungOh <atcnature@gmail.com>
2025-07-19 23:54:02 +0900

    [feature] 쿼터뷰/숄더뷰 전환 적용

---
+++ Source/ArenaBattle/Character/ABCharacterControlData.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ABCharacterControlData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API UABCharacterControlData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UABCharacterControlData();
+
+	UPROPERTY(EditAnywhere, Category = Pawn)
+	uint32 bUseControllerRotationYaw : 1;
+
+	UPROPERTY(EditAnywhere, Category = CharacterMovement)
+	uint32 bOrientRotationToMovement : 1;
+
+	UPROPERTY(EditAnywhere, Category = CharacterMovement)
+	uint32 bUseControllerDesiredRotation : 1;
+
+	UPROPERTY(EditAnywhere, Category = CharacterMovement)
+	FRotator RotationRate;
+
+	// 앞으로 사용할 입력 매핑 컨텍스트 담을 멤버 변수
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = CharacterMovement)
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = SpringArm)
+	float TargetArmLength;
+
+	UPROPERTY(EditAnywhere, Category = SpringArm)
+	FRotator RelativeRotation;
+
+	UPROPERTY(EditAnywhere, Category = SpringArm)
+	uint32 bUsePawnControlRotation : 1;
+
+	UPROPERTY(EditAnywhere, Category = SpringArm)
+	uint32 bInheritPitch : 1;
+
+	UPROPERTY(EditAnywhere, Category = SpringArm)
+	uint32 bInheritYaw : 1;
+
+	UPROPERTY(EditAnywhere, Category = SpringArm)
+	uint32 bInheritRoll : 1;
+
+	UPROPERTY(EditAnywhere, Category = SpringArm)
+	uint32 bDoCollisionTest : 1;
+};
