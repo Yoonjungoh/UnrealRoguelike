14691b92298a78f966a431d2128fae71d503cd96
YoonJungOh <atcnature@gmail.com>
2025-07-24 01:12:57 +0900

    [feat] 상자 기믹 만든 후 트리거 적용 (닿으면 이펙트 뜨면서 상자 안보이게 처리, 아이템 획득 처리 필요함)

---
+++ Source/ArenaBattle/Item/ABItemBox.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/ABItemBox.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Physics/ABCollision.h"
+
+// Sets default values
+AABItemBox::AABItemBox()
+{
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Effect = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Effect"));
+
+	RootComponent = Trigger;
+	Mesh->SetupAttachment(Trigger);
+	Effect->SetupAttachment(Trigger);
+
+	Trigger->SetCollisionProfileName(CPROFILE_ABTRIGGER);
+	Trigger->SetBoxExtent(FVector(40.0f,42.0f,30.0f));	// 박스 사이즈
+	// 아래의 델리게이트 바인딩을 통해서 트리거가 발동되면서 매핑된 함수가 실행됨
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &AABItemBox::OnOverlapBegin);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> BoxMeshRef(TEXT("/Script/Engine.StaticMesh'/Game/ArenaBattle/Environment/Props/SM_Env_Breakables_Box1.SM_Env_Breakables_Box1'"));
+	if (BoxMeshRef.Object)
+	{
+		Mesh->SetStaticMesh(BoxMeshRef.Object);
+	}
+	Mesh->SetRelativeLocation(FVector(0.0f, -3.5f, -30.0f));
+	Mesh->SetCollisionProfileName(TEXT("NoCollision"));
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> EffectRef(TEXT("/Script/Engine.ParticleSystem'/Game/ArenaBattle/Effect/P_TreasureChest_Open_Mesh.P_TreasureChest_Open_Mesh'"));
+	if (EffectRef.Object)
+	{
+		Effect->SetTemplate(EffectRef.Object);
+		Effect->bAutoActivate = false;	// 처음에 바로 발동하는 것을 방지
+	}
+
+}
+
+void AABItemBox::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult)
+{
+	Effect->Activate(true);
+	Mesh->SetHiddenInGame(true);
+	SetActorEnableCollision(false);
+	// 이펙트 끝났을 때, 호출하는 함수 매핑
+	Effect->OnSystemFinished.AddDynamic(this, &AABItemBox::OnEffectFinished);
+}
+
+void AABItemBox::OnEffectFinished(UParticleSystemComponent* ParticleSystem)
+{
+	Destroy();
+}
---
+++ Source/ArenaBattle/Item/ABItemBox.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ABItemBox.generated.h"
+
+UCLASS()
+class ARENABATTLE_API AABItemBox : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AABItemBox();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = Box)
+	TObjectPtr<class UBoxComponent> Trigger;
+
+	UPROPERTY(VisibleAnywhere, Category = Box)
+	TObjectPtr<class UStaticMeshComponent> Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category = Effect)
+	TObjectPtr<class UParticleSystemComponent> Effect;
+
+	UFUNCTION()	// 블루프린트에서도 사용할 수 있게 다이나믹 델리게이트로 설정돼 있어서 UFUNCTION 지정해줘야 함
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult);
+
+	UFUNCTION()
+		void OnEffectFinished(class UParticleSystemComponent* ParticleSystem);
+};
