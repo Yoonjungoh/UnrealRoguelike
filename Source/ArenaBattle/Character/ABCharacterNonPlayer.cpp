84b484d1bacc31045162be4382e0646ea01c7829
YoonJungOh <atcnature@gmail.com>
2025-07-25 03:39:25 +0900

    [feat] 무기 능력치 적용 및 랜덤 몬스터 생성 적용

--- Source/ArenaBattle/Character/ABCharacterNonPlayer.cpp
+++ Source/ArenaBattle/Character/ABCharacterNonPlayer.cpp
@@ -2,9 +2,21 @@
 
 
 #include "Character/ABCharacterNonPlayer.h"
+#include "Engine/AssetManager.h"
 
 AABCharacterNonPlayer::AABCharacterNonPlayer()
 {
+	GetMesh()->SetHiddenInGame(true);	// 메시를 우선 숨기고 로딩 완료되면 보이게 할 예정
+}
+
+void AABCharacterNonPlayer::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	ensure(NPCMeshes.Num() > 0);
+	int32 RandIndex = FMath::RandRange(0, NPCMeshes.Num() - 1);
+	NPCMeshHandle = UAssetManager::Get().GetStreamableManager().RequestAsyncLoad(NPCMeshes[RandIndex], FStreamableDelegate::CreateUObject(this, &AABCharacterNonPlayer::NPCMeshLoadCompleted));
+	
 }
 
 void AABCharacterNonPlayer::SetDead()
@@ -18,4 +30,19 @@ void AABCharacterNonPlayer::SetDead()
 			Destroy();
 		}
 	), DeadEventDelayTime, false);
+}
+
+void AABCharacterNonPlayer::NPCMeshLoadCompleted()
+{
+	if (NPCMeshHandle.IsValid())
+	{
+		USkeletalMesh* NPCMesh = Cast<USkeletalMesh>(NPCMeshHandle->GetLoadedAsset());
+		if (NPCMesh)
+		{
+			GetMesh()->SetSkeletalMesh(NPCMesh);
+			GetMesh()->SetHiddenInGame(false);	// 로딩 완료됐으니 보이게 해도 됨
+		}
+	}
+
+	NPCMeshHandle->ReleaseHandle();
 }
